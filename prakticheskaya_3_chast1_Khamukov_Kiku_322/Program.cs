9c5b7caa21c7eb66b1a322350e5a17a12c9edda2
Тимур Хамуков <worlsait@gmail.com>
2025-02-21 23:42:38 -0100

    ПР1 Ч1 завершена успешно.

--- prakticheskaya_3_chast1_Khamukov_Kiku_322/Program.cs
+++ prakticheskaya_3_chast1_Khamukov_Kiku_322/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace prakticheskaya_3_chast1_Khamukov_Kiku_322
+int result = Fibonacci(5);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
 {
-    internal class Program
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
     {
-        static void Main(string[] args)
-        {
-        }
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
     }
+
+    return n == 0 ? n1 : n2;
 }
